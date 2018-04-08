2fa1c2c99ee46a19c8e8957480d945768eed28d3
Michael Walczyk <michael.walczyk@obscuradigital.com>
2018-04-07 16:10:38 -0800

    Moves to builder syntax.

--- vkstarter/main.cpp
+++ vkstarter/main.cpp
@@ -22,7 +22,6 @@ vk::UniqueShaderModule load_spv_into_module(const vk::UniqueDevice& device, cons
 
 	size_t file_size = static_cast<size_t>(file.tellg());
 	std::vector<char> buffer(file_size);
-
 	file.seekg(0);
 	file.read(buffer.data(), file_size);
 	file.close();
@@ -32,14 +31,7 @@ vk::UniqueShaderModule load_spv_into_module(const vk::UniqueDevice& device, cons
 	return device->createShaderModuleUnique(shader_module_create_info);
 }
 
-static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(VkDebugReportFlagsEXT flags,
-													 VkDebugReportObjectTypeEXT object_type,
-													 uint64_t object,
-													 size_t location,
-													 int32_t code,
-													 const char* layer_prefix,
-													 const char* msg,
-													 void* user_data) 
+VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT object_type, uint64_t object, size_t location, int32_t code, const char* layer_prefix, const char* msg, void* user_data) 
 {
 	std::ostringstream message;
 
@@ -64,7 +56,6 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(VkDebugReportFlagsEXT flags
 		message << "DEBUG: ";
 	}
 	message << "[" << layer_prefix << "] Code " << code << " : " << msg;
-
 	std::cout << message.str() << std::endl;
 
 	return VK_FALSE;
@@ -87,21 +78,13 @@ int main()
 	layers.push_back("VK_LAYER_LUNARG_standard_validation");
 #endif
 	auto application_info = vk::ApplicationInfo{ app_name.c_str(), VK_MAKE_VERSION(1, 0, 0), app_name.c_str(), VK_MAKE_VERSION(1, 0, 0), VK_API_VERSION_1_1 };
-	auto instance_create_info = vk::InstanceCreateInfo{};
-	instance_create_info.pApplicationInfo = &application_info;
-	instance_create_info.ppEnabledLayerNames = layers.data();
-	instance_create_info.enabledLayerCount = static_cast<uint32_t>(layers.size());
-	instance_create_info.ppEnabledExtensionNames = extensions.data();
-	instance_create_info.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
-
-	auto instance = vk::createInstanceUnique(instance_create_info);
+	
+	auto instance = vk::createInstanceUnique(vk::InstanceCreateInfo{ {}, &application_info, static_cast<uint32_t>(layers.size()), layers.data(), static_cast<uint32_t>(extensions.size()), extensions.data() });
 	
 	// Debug report callback
 #ifdef _DEBUG
 	auto dynamic_dispatch_loader = vk::DispatchLoaderDynamic{ instance.get() };
-	auto debug_report_callback_create_info = vk::DebugReportCallbackCreateInfoEXT{};
-	debug_report_callback_create_info.flags = vk::DebugReportFlagBitsEXT::eError | vk::DebugReportFlagBitsEXT::eWarning;
-	debug_report_callback_create_info.pfnCallback = debug_callback;
+	auto debug_report_callback_create_info = vk::DebugReportCallbackCreateInfoEXT{ vk::DebugReportFlagBitsEXT::eError | vk::DebugReportFlagBitsEXT::eWarning, debug_callback };
 	
 	auto debug_report_callback = instance->createDebugReportCallbackEXT(debug_report_callback_create_info, nullptr, dynamic_dispatch_loader);
 	std::cout << "Initializing debug report callback\n";
@@ -122,20 +105,21 @@ int main()
 	queue_create_info.queueFamilyIndex = static_cast<uint32_t>(std::distance(queue_family_properties.begin(), std::find_if(queue_family_properties.begin(), queue_family_properties.end(),
 			[](const vk::QueueFamilyProperties& item) { return item.queueFlags & vk::QueueFlagBits::eGraphics; }
 	)));
-	std::cout << "Queue family at index [ " << queue_create_info.queueFamilyIndex << " ] supports graphics\n";
+	std::cout << "Using queue family at index [ " << queue_create_info.queueFamilyIndex << " ], which supports graphics operations\n";
 
 	// Logical device
 	const std::vector<const char*> device_extensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
-	auto device_create_info = vk::DeviceCreateInfo{};
-	device_create_info.pQueueCreateInfos = &queue_create_info;
-	device_create_info.queueCreateInfoCount = 1;
-	device_create_info.ppEnabledExtensionNames = device_extensions.data();
-	device_create_info.enabledExtensionCount = static_cast<uint32_t>(device_extensions.size());
+	auto device_create_info = vk::DeviceCreateInfo{}
+		.setPQueueCreateInfos(&queue_create_info)
+		.setQueueCreateInfoCount(1)
+		.setPpEnabledExtensionNames(device_extensions.data())
+		.setEnabledExtensionCount(static_cast<uint32_t>(device_extensions.size()));
 
 	auto device = physical_device.createDeviceUnique(device_create_info);
 
 	// Queue
-	auto queue = device->getQueue(queue_create_info.queueFamilyIndex, 0);
+	const uint32_t queue_index = 0;
+	auto queue = device->getQueue(queue_create_info.queueFamilyIndex, queue_index);
 
 	// Surface
 	auto surface_create_info = vk::Win32SurfaceCreateInfoKHR{ {}, GetModuleHandle(nullptr), glfwGetWin32Window(window) };
@@ -148,18 +132,18 @@ int main()
 	auto surface_support = physical_device.getSurfaceSupportKHR(queue_create_info.queueFamilyIndex, surface.get());
 
 	// Swapchain
-	const auto swapchain_image_format = vk::Format::eB8G8R8A8Unorm;
-	const vk::Extent2D swapchain_extent = { width, height };
-	auto swapchain_create_info = vk::SwapchainCreateInfoKHR{};
-	swapchain_create_info.presentMode = vk::PresentModeKHR::eMailbox;
-	swapchain_create_info.imageExtent = swapchain_extent;
-	swapchain_create_info.imageFormat = swapchain_image_format;
-	swapchain_create_info.imageArrayLayers = 1;
-	swapchain_create_info.imageUsage = vk::ImageUsageFlagBits::eColorAttachment;
-	swapchain_create_info.minImageCount = surface_capabilities.minImageCount + 1;
-	swapchain_create_info.preTransform = surface_capabilities.currentTransform;
-	swapchain_create_info.clipped = true;
-	swapchain_create_info.surface = surface.get();
+	const auto swapchain_image_format{ vk::Format::eB8G8R8A8Unorm };
+	const auto swapchain_extent = vk::Extent2D{ width, height };
+	auto swapchain_create_info = vk::SwapchainCreateInfoKHR{}
+		.setPresentMode(vk::PresentModeKHR::eMailbox)
+		.setImageExtent(swapchain_extent)
+		.setImageFormat(swapchain_image_format)
+		.setImageArrayLayers(1)
+		.setImageUsage(vk::ImageUsageFlagBits::eColorAttachment)
+		.setMinImageCount(surface_capabilities.minImageCount + 1)
+		.setPreTransform(surface_capabilities.currentTransform)
+		.setClipped(true)
+		.setSurface(surface.get());
 
 	auto swapchain = device->createSwapchainKHRUnique(swapchain_create_info);
 
@@ -169,57 +153,48 @@ int main()
 	std::vector<vk::UniqueImageView> swapchain_image_views;
 	for (const auto& image : swapchain_images)
 	{
-		auto image_view_create_info = vk::ImageViewCreateInfo{};
-		image_view_create_info.image = image;
-		image_view_create_info.viewType = vk::ImageViewType::e2D;
-		image_view_create_info.format = swapchain_image_format;
-		image_view_create_info.subresourceRange.aspectMask = vk::ImageAspectFlagBits::eColor;
-		image_view_create_info.subresourceRange.levelCount = 1;
-		image_view_create_info.subresourceRange.layerCount = 1;
-
+		const auto subresource_range = vk::ImageSubresourceRange{ vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1 };
+		auto image_view_create_info = vk::ImageViewCreateInfo{ {}, image, vk::ImageViewType::e2D, swapchain_image_format, {}, subresource_range };
 		swapchain_image_views.push_back(device->createImageViewUnique(image_view_create_info));
 	}
 	std::cout << "Created [ " << swapchain_image_views.size() << " ] image views\n";
 
 	// Shader modules 
-	auto vs_module = load_spv_into_module(device, "vert.spv");
-	auto fs_module = load_spv_into_module(device, "frag.spv");
+	const std::string path_prefix = "";
+	const std::string vs_spv_path = path_prefix + "vert.spv";
+	const std::string fs_spv_path = path_prefix + "frag.spv";
+	auto vs_module = load_spv_into_module(device, vs_spv_path);
+	auto fs_module = load_spv_into_module(device, fs_spv_path);
 	std::cout << "Successfully loaded shader modules\n";
 
 	// Pipeline layout
-	auto pipeline_layout = device->createPipelineLayoutUnique({});
+	auto pipeline_layout = device->createPipelineLayoutUnique({ /* Add push constants and descriptor sets here */ });
 
 	// Render pass
-	auto attachment_description = vk::AttachmentDescription{};
-	attachment_description.format = swapchain_image_format;
-	attachment_description.loadOp = vk::AttachmentLoadOp::eClear;
-	attachment_description.storeOp = vk::AttachmentStoreOp::eStore;
-	attachment_description.stencilLoadOp = vk::AttachmentLoadOp::eDontCare;
-	attachment_description.stencilStoreOp = vk::AttachmentStoreOp::eDontCare;
-	attachment_description.finalLayout = vk::ImageLayout::ePresentSrcKHR;
+	auto attachment_description = vk::AttachmentDescription{}
+		.setFormat(swapchain_image_format)
+		.setLoadOp(vk::AttachmentLoadOp::eClear)
+		.setStoreOp(vk::AttachmentStoreOp::eStore)
+		.setStencilLoadOp(vk::AttachmentLoadOp::eDontCare)
+		.setStencilStoreOp(vk::AttachmentStoreOp::eDontCare)
+		.setFinalLayout(vk::ImageLayout::ePresentSrcKHR);
 
 	const uint32_t attachment_index = 0;
 	auto attachment_reference = vk::AttachmentReference{ attachment_index, vk::ImageLayout::eColorAttachmentOptimal };
 	
-	auto subpass_description = vk::SubpassDescription{};
-	subpass_description.pColorAttachments = &attachment_reference;
-	subpass_description.colorAttachmentCount = 1;
+	auto subpass_description = vk::SubpassDescription{}
+		.setPColorAttachments(&attachment_reference)
+		.setColorAttachmentCount(1);
 
-	auto subpass_dependency = vk::SubpassDependency{};
-	subpass_dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
-	subpass_dependency.dstSubpass = 0;
-	subpass_dependency.srcStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput;
-	subpass_dependency.srcAccessMask = {};
-	subpass_dependency.dstStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput;
-	subpass_dependency.dstAccessMask = vk::AccessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eColorAttachmentWrite;
+	auto subpass_dependency = vk::SubpassDependency{}
+		.setSrcSubpass(VK_SUBPASS_EXTERNAL)
+		.setDstSubpass(0)
+		.setSrcStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput)
+		.setSrcAccessMask({})
+		.setDstStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput)
+		.setDstAccessMask(vk::AccessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eColorAttachmentWrite);
 
-	auto render_pass_create_info = vk::RenderPassCreateInfo{};
-	render_pass_create_info.pAttachments = &attachment_description;
-	render_pass_create_info.attachmentCount = 1;
-	render_pass_create_info.pSubpasses = &subpass_description;
-	render_pass_create_info.subpassCount = 1;
-	render_pass_create_info.pDependencies = &subpass_dependency;
-	render_pass_create_info.dependencyCount = 1;
+	auto render_pass_create_info = vk::RenderPassCreateInfo{ {}, 1, &attachment_description, 1, &subpass_description, 1, &subpass_dependency };
 
 	auto render_pass = device->createRenderPassUnique(render_pass_create_info);
 
@@ -230,61 +205,59 @@ int main()
 	const vk::PipelineShaderStageCreateInfo shader_stage_create_infos[2] = { vs_stage_create_info, fs_stage_create_info };
 
 	auto vertex_input_state_create_info = vk::PipelineVertexInputStateCreateInfo{};
+
 	auto input_assembly_create_info = vk::PipelineInputAssemblyStateCreateInfo{ {}, vk::PrimitiveTopology::eTriangleList };
 
-	const vk::Viewport viewport{ 0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height), 0.0f, 1.0f };
+	const float min_depth = 0.0f;
+	const float max_depth = 1.0f;
+	const vk::Viewport viewport{ 0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height), min_depth, max_depth };
 	const vk::Rect2D scissor{ { 0, 0 }, swapchain_extent };
-	auto viewport_state_create_info = vk::PipelineViewportStateCreateInfo{};
-	viewport_state_create_info.pViewports = &viewport;
-	viewport_state_create_info.viewportCount = 1;
-	viewport_state_create_info.pScissors = &scissor;
-	viewport_state_create_info.scissorCount = 1;
+	auto viewport_state_create_info = vk::PipelineViewportStateCreateInfo{ {}, 1, &viewport, 1, &scissor };
 
-	auto rasterization_state_create_info = vk::PipelineRasterizationStateCreateInfo{};
-	rasterization_state_create_info.frontFace = vk::FrontFace::eClockwise;
-	rasterization_state_create_info.cullMode = vk::CullModeFlagBits::eBack;
-	rasterization_state_create_info.lineWidth = 1.0f;
+	auto rasterization_state_create_info = vk::PipelineRasterizationStateCreateInfo{}
+		.setFrontFace(vk::FrontFace::eClockwise)
+		.setCullMode(vk::CullModeFlagBits::eBack)
+		.setLineWidth(1.0f);
 
 	auto multisample_state_create_info = vk::PipelineMultisampleStateCreateInfo{};
 
-	auto color_blend_attachment_state = vk::PipelineColorBlendAttachmentState{};
-	color_blend_attachment_state.colorWriteMask = vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG | vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA;
+	auto color_blend_attachment_state = vk::PipelineColorBlendAttachmentState{}
+		.setColorWriteMask(vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG | vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA);
 
-	auto color_blend_state_create_info = vk::PipelineColorBlendStateCreateInfo{};
-	color_blend_state_create_info.attachmentCount = 1;
-	color_blend_state_create_info.pAttachments = &color_blend_attachment_state;
+	auto color_blend_state_create_info = vk::PipelineColorBlendStateCreateInfo{}
+		.setPAttachments(&color_blend_attachment_state)
+		.setAttachmentCount(1);
 
-	auto graphics_pipeline_create_info = vk::GraphicsPipelineCreateInfo{};
-	graphics_pipeline_create_info.layout = pipeline_layout.get();
-	graphics_pipeline_create_info.renderPass = render_pass.get();
-	graphics_pipeline_create_info.pStages = shader_stage_create_infos;
-	graphics_pipeline_create_info.stageCount = 2;
-	graphics_pipeline_create_info.pVertexInputState = &vertex_input_state_create_info;
-	graphics_pipeline_create_info.pInputAssemblyState = &input_assembly_create_info;
-	graphics_pipeline_create_info.pViewportState = &viewport_state_create_info;
-	graphics_pipeline_create_info.pRasterizationState = &rasterization_state_create_info;
-	graphics_pipeline_create_info.pMultisampleState = &multisample_state_create_info;
-	graphics_pipeline_create_info.pColorBlendState = &color_blend_state_create_info;
+	auto graphics_pipeline_create_info = vk::GraphicsPipelineCreateInfo{ 
+		{}, 
+		2, 
+		shader_stage_create_infos, 
+		&vertex_input_state_create_info,
+		&input_assembly_create_info, 
+		nullptr, /* Add tessellation state here */
+		&viewport_state_create_info, 
+		&rasterization_state_create_info,
+		&multisample_state_create_info, 
+		nullptr, /* Add depth stencil state here */
+		&color_blend_state_create_info, 
+		nullptr, /* Add dynamic state here */ 
+		pipeline_layout.get(), 
+		render_pass.get() 
+	};
 
 	auto graphics_pipeline = device->createGraphicsPipelineUnique({}, graphics_pipeline_create_info);
 	std::cout << "Created graphics pipeline\n";
 
 	// Framebuffers
+	const uint32_t framebuffer_layers = 1;
 	std::vector<vk::UniqueFramebuffer> framebuffers;
 	for (const auto& image_view : swapchain_image_views)
 	{
-		auto framebuffer_create_info = vk::FramebufferCreateInfo{};
-		framebuffer_create_info.pAttachments = &image_view.get();
-		framebuffer_create_info.attachmentCount = 1;
-		framebuffer_create_info.renderPass = render_pass.get();
-		framebuffer_create_info.width = width;
-		framebuffer_create_info.height = height;
-		framebuffer_create_info.layers = 1;
-		std::cout << "Creating framebuffer\n";
-
+		auto framebuffer_create_info = vk::FramebufferCreateInfo{ {}, render_pass.get(), 1, &image_view.get(), width, height, framebuffer_layers };
 		framebuffers.push_back(device->createFramebufferUnique(framebuffer_create_info));
 	}
-	
+	std::cout << "Created [ " << framebuffers.size() << " ] framebuffers\n";
+
 	// Command pool
 	auto command_pool = device->createCommandPoolUnique(vk::CommandPoolCreateInfo{ {}, queue_create_info.queueFamilyIndex });
 
@@ -292,8 +265,8 @@ int main()
 	auto command_buffers = device->allocateCommandBuffersUnique(vk::CommandBufferAllocateInfo{ command_pool.get(), vk::CommandBufferLevel::ePrimary, static_cast<uint32_t>(framebuffers.size()) });
 	std::cout << "Allocated [ " << command_buffers.size() << " ] command buffers\n";
 
-	const vk::ClearValue clear = std::array<float, 4>{ 0.0f, 0.0f, 0.0f, 1.0f };
-	const vk::Rect2D render_area = { { 0, 0 }, swapchain_extent };
+	const vk::ClearValue clear = std::array<float, 4>{ 0.15f, 0.15f, 0.15f, 1.0f };
+	const vk::Rect2D render_area{ { 0, 0 }, swapchain_extent };
 	for (size_t i = 0; i < command_buffers.size(); ++i)
 	{	
 		command_buffers[i]->begin(vk::CommandBufferBeginInfo{ vk::CommandBufferUsageFlagBits::eSimultaneousUse });
@@ -315,25 +288,12 @@ int main()
 
 		auto index = device->acquireNextImageKHR(swapchain.get(), (std::numeric_limits<uint64_t>::max)(), semaphore_image_available.get(), {});
 
-		vk::PipelineStageFlags wait_stages[] = { vk::PipelineStageFlagBits::eColorAttachmentOutput };
-		auto submit_info = vk::SubmitInfo{};
-		submit_info.pWaitSemaphores = &semaphore_image_available.get();
-		submit_info.waitSemaphoreCount = 1;
-		submit_info.pWaitDstStageMask = wait_stages;
-		submit_info.pSignalSemaphores = &sempahore_render_finished.get();
-		submit_info.signalSemaphoreCount = 1;
-		submit_info.pCommandBuffers = &command_buffers[index.value].get();
-		submit_info.commandBufferCount = 1;
+		const vk::PipelineStageFlags wait_stages[] = { vk::PipelineStageFlagBits::eColorAttachmentOutput };
 
+		auto submit_info = vk::SubmitInfo{ 1, &semaphore_image_available.get(), wait_stages, 1, &command_buffers[index.value].get(), 1, &sempahore_render_finished.get() };
 		queue.submit(submit_info, {});
 
-		auto present_info = vk::PresentInfoKHR{};
-		present_info.pWaitSemaphores = &sempahore_render_finished.get();
-		present_info.waitSemaphoreCount = 1;
-		present_info.pSwapchains = &swapchain.get();
-		present_info.swapchainCount = 1;
-		present_info.pImageIndices = &index.value;
-
+		auto present_info = vk::PresentInfoKHR{ 1, &sempahore_render_finished.get(), 1, &swapchain.get(), &index.value };
 		queue.presentKHR(present_info);
 	}
 
--- vkstarter/shader.vert
+++ vkstarter/shader.vert
@@ -8,13 +8,13 @@ out gl_PerVertex
 
 layout(location = 0) out vec3 color;
 
-vec2 positions[3] = vec2[](vec2( 0.0, -0.5),
-						   vec2( 0.5,  0.5),
-						   vec2(-0.5,  0.5));
+const vec2 positions[3] = vec2[](vec2( 0.0, -0.5),
+						         vec2( 0.5,  0.5),
+						         vec2(-0.5,  0.5));
 
-vec3 colors[3] = vec3[](vec3(1.0, 0.0, 0.0),
-					    vec3(0.0, 1.0, 0.0),
-					    vec3(0.0, 0.0, 1.0));
+const vec3 colors[3] = vec3[](vec3(1.0, 0.0, 0.0),
+					          vec3(0.0, 1.0, 0.0),
+					          vec3(0.0, 0.0, 1.0));
 
 void main() 
 {
